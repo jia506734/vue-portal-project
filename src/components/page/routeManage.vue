573abfa41ae18cc248a32f1466b3ff24933ee407
jia506734 <739243433@qq.com>
2018-07-20 23:45:41 +0800

    sss

--- src/components/page/routeManage.vue
+++ src/components/page/routeManage.vue
@@ -1,5 +1,41 @@
 <template>
-<div style="height:100%;">
-线路管理
+  <div style="height:100%;">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="我的" name="my">
+        <common-route :type="activeName"></common-route>
+      </el-tab-pane>
+      <el-tab-pane label="同业" name="trade">
+        <common-route :type="activeName"></common-route>
+      </el-tab-pane>
+      <el-tab-pane label="网店" name="store">
+        <common-route :type="activeName"></common-route>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
+
+<script>
+import commonRoute from '../common/commonRoute'
+export default {
+  data(){
+    return{
+      activeName:'my',
+    }
+  },
+    components:{
+      commonRoute,
+  },
+  methods:{
+      handleClick(tab, event) {
+        if(this.activeName=="my"){
+          
+        }else if(this.activeName=="trade"){
+
+        }else if(this.activeName=="store"){
+
+        }
+      }
+  },
+}
+</script>
+
