dd113b9566db53769feb730ce434fab802bd4090
jia506734 <31120271+jia506734@users.noreply.github.com>
2018-09-08 23:23:21 +0800

    线路详情入口

---
+++ src/components/page/showRouteDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div><manage></manage></div>
+</template>
+
+<script>
+import manage from "../manage"
+export default {
+    data(){
+        return{
+
+        }
+    },
+    components:{
+        manage
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
+
